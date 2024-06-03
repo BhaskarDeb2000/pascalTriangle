8a4fa77eb671de3bd26b3e5e65b130ee91e8801a
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-06-03 09:24:28 +0300

    feat: pascal's triangle

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, i, j, coeff = 1;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            if (j == 0 || i == 0)
+                coeff = 1;
+            else
+                coeff = coeff * (i - j + 1) / j;
+            printf("%d ", coeff);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Pascal's Triangle
+
+## Description
+
+In this app, all user has to do is input the number of Row one wants to see. Everything will happen automatically.
+
+This is the formula the code is using: coef\*(i-j+1)/j
+
+# App Demo
+
+Here is a demo of the app:
+
+![AppDemo](./demoVid/Screen%20Recording%202024-06-03%20at%209.15.23.gif)
